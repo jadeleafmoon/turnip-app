0ab360ce33e88fbc6e3be6c08ff43ccbb880aa3c
jadeleafmoon <jadeleafmoon@gmail.com>
2022-05-17 23:57:33 +0900

    update: update 01-products seed

--- db/seeds/01-products.js
+++ db/seeds/01-products.js
@@ -6,8 +6,9 @@ exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('products').del()
   await knex('products').insert([
-    {id: 1, name: 'Glasses', price: 15},
-    {id: 2, name: 'Hat', price: 10.50},
-    {id: 3, name: 'Desk', price: 200.99},
+    {name: 'Hat', price: 2},
+    {name: 'Glasses', price: 15},
+    {name: 'Shoes', price: 10.50},
+    {name: 'TV', price: 200.99},
   ]);
 };
