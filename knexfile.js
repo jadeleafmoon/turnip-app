6cd494bdf76200944599bb3696a4e0af0b28a1b6
jadeleafmoon <jadeleafmoon@gmail.com>
2022-05-18 20:08:53 +0900

    fix: update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -3,17 +3,17 @@ require('dotenv').config();
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
-// 
+//
 
 module.exports = {
 	client     : 'pg',
-	connection : {
+	connection : process.env.DATABASE_URL || {
 		host     : process.env.DB_HOST,
 		port     : process.env.DB_PORT,
 		database : process.env.DB_NAME,
 		user     : process.env.DB_USER,
 		password : process.env.DB_PASSWORD,
-		charset: 'utf8'
+		charset  : 'utf8'
 	},
 	migrations : {
 		directory : './db/migrations',
