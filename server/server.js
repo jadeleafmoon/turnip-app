b0ba714a2e59b4daff3e7e52036652b45242f5d5
jadeleafmoon <jadeleafmoon@gmail.com>
2022-05-18 19:42:31 +0900

    wip

--- server/server.js
+++ server/server.js
@@ -9,9 +9,21 @@ const knex = require('./knex');
 
 app.get('/items', (req, res) => {
 	knex('items').returning('*').then((data) => {
-		console.log('🔥 Server', data);
+		// console.log('🔥 Server', data);
 		res.send(data);
 	});
+});
+
+app.post('/items', (req, res) => {
+	console.log('🔥 POST /items req', req.body);
+	knex('items').insert(req.body);
+	// await knex('items').insert([
+	//   {name: 'Glasses', price: 20},
+	//   {name: 'Shoes', price: 10.50},
+	//   {name: 'TV', price: 200.99},
+	//   {name: 'Hat', price: 2},
+	// ]);
+	res.send("Done!");
 });
 
 app.listen(PORT, console.log(`You are listening on port ${PORT}`));
