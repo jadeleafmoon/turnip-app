1183492739afe23a8f4e0bbc27d46174da9cf548
jadeleafmoon <jadeleafmoon@gmail.com>
2022-07-11 18:23:26 +0900

    feat: add Card template

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,17 @@
+import { eventWrapper } from "@testing-library/user-event/dist/utils";
 import React from "react";
 
 export const Card = () => {
-  return <div>Card</div>;
+  return (
+    <div className="card">
+      <div className="card-body">
+        <img className="card-image"/>
+        <h2 className="card-title"></h2>
+        <p className="card-description"></p>
+      </div>
+      <button className="card-button"></button>
+
+    </div>
+
+  );
 };
