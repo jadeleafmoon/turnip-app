63ef6cf49f5027f7e0a2bcef9e1822485a3b49e4
jadeleafmoon <jadeleafmoon@gmail.com>
2022-07-11 19:21:00 +0900

    feat: add cards to My Profile page

--- src/components/AllItems.js
+++ src/components/AllItems.js
@@ -20,26 +20,6 @@ const AllItems = (props) => {
       })}
     </div>
   );
-
-  // return (
-  // 	<section className="item-list">
-  // 		<div key={uuidv4()} className="item-card items-top-row">
-  // 			<span>Owner</span>
-  // 			<span>Item Name</span>
-  // 			<span>Price</span>
-  // 		</div>
-  // 		{items.map((item) => {
-  // 			return (
-  // 				<div key={uuidv4()} className="item-card">
-  // 					<span>{item.owner}</span>
-  // 					<span>{item.name}</span>
-  // 					<span>{item.price}</span>
-  // 					<button onClick={() => handleClickViewButton(item)}>View</button>
-  // 				</div>
-  // 			);
-  // 		})}
-  // 	</section>
-  // );
 };
 
 export default AllItems;
