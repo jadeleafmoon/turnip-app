bfa78f2d4d2d8c6a76b3d47f71cd004f3ee4c09b
jadeleafmoon <jadeleafmoon@gmail.com>
2022-07-13 01:56:03 +0900

    feat: add working upload and download with firebase

--- src/components/AllItems.js
+++ src/components/AllItems.js
@@ -4,15 +4,21 @@ import { v4 as uuidv4 } from "uuid";
 
 const AllItems = (props) => {
   const { items, handleClickViewButton } = props;
+  const defaultUrl =
+    "https://images.unsplash.com/photo-1517336714731-489689fd1ca8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1326&q=80";
+
   useEffect(() => {
     console.log("\n 🍎 items:", items[0]);
   }, []);
+
   return (
     <div className="card-wrapper">
       {items.map((item) => {
+        console.log('\n 🍎 item in AllItems:', item);
         return (
           <Card
-            img="https://images.unsplash.com/photo-1517336714731-489689fd1ca8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1326&q=80"
+            // img="https://images.unsplash.com/photo-1517336714731-489689fd1ca8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1326&q=80"
+            img={item?.imageUrl || defaultUrl}
             title={item.name}
             description={item.description}
             price={item.price}
